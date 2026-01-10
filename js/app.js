c0ae45813a4ca606e91906159f5423080ff015b6
Fotimaizahro <fotimaqoldasheva149@gmail.com>
2026-01-10 20:39:09 +0500

    ozgardi10

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
+      },
+    });
